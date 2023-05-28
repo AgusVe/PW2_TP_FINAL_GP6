a8921100fff4463f5b19348d251196a8eac98c3e
Luis Agustin Vega Dobal <lvegadobal@alumno.unlam.edu.ar>
2023-05-28 18:55:09 -0300

    validacion en el home

--- controller/SesionController.php
+++ controller/SesionController.php
@@ -3,16 +3,40 @@
 class SesionController{
 
     private $sesionModel;
+    private $renderer;
 
-    public function __construct($sesionModel){
+    public function __construct($sesionModel,$renderer){
         $this->sesionModel = $sesionModel;
+        $this->renderer = $renderer;
+
+
     }
 
     public function iniciarSesion(){
         if(isset($_POST['email']) && isset ($_POST['password'])){
+            $errors = array();
+
+
             $email= $_POST['email'];
             $pass = $_POST['password'];
+            $emailValidado=$this->sesionModel->validarEmail($email);
+            if($emailValidado==1){
+                $errors['email'] = 'El email ingresado no coincide con usuario registrado';
+            }
+
             $resultado = $this->sesionModel->validar($email,$pass);
+
+            if($resultado==1){
+                $errors['contraseña'] = 'La contraseña ingresada no coincide con usuario registrado';
+            }
+            // VERIFICO SI HAY ERRORES Y LOS MANDO A LA VISTA
+            if (count($errors) > 0) {
+                $erroresEncontrados = $errors;
+
+                $data = array('errors' => $erroresEncontrados);
+                $this->renderer->render("home", $data);
+                exit;
+            }
             if($resultado){
                 $rol = $resultado["0"]["idRol"];
                 $_SESSION['email']= $resultado["0"]["email"];
--- model/SesionModel.php
+++ model/SesionModel.php
@@ -1,22 +1,42 @@
 <?php
 
-class SesionModel{
+class SesionModel
+{
 
     private $database;
-    public function __construct($database){
+
+    public function __construct($database)
+    {
         $this->database = $database;
     }
 
-    public function validar($email, $clave){
-        $consultaContra = "SELECT * FROM usuario WHERE email='".$email."' AND estado='1'";
-        $saveQuery=$this->database->query($consultaContra);
-        $HashObtenido=$saveQuery["0"]["contrasenia"];
+    public function validar($email, $clave)
+    {
+        $consultaContra = "SELECT contrasenia FROM usuario WHERE email='" . $email . "' AND estado='1'";
+        $saveQuery = $this->database->query($consultaContra);
 
-        if(password_verify($clave,$HashObtenido)) {
-            $sql = "SELECT *  FROM usuario WHERE email='".$email."' AND contrasenia='" . $HashObtenido ."' AND estado='1'";
-            return $this->database->query($sql);
+        $HashObtenido = $saveQuery;
+        if ($HashObtenido != null) {
+            if (password_verify($clave, $HashObtenido)) {
+                $sql = "SELECT *  FROM usuario WHERE email='" . $email . "' AND contrasenia='" . $HashObtenido . "' AND estado='1'";
+                return $this->database->query($sql);
 
+            }
+        } else {
+            return 1;
         }
-        return null;
+    }
+
+    public function validarEmail($email)
+    {
+        $consultaEmail = "SELECT email FROM usuario WHERE email='" . $email . "' AND estado='1'";
+        $saveQuery = $this->database->query($consultaEmail);
+
+        if ($saveQuery) {
+            return $saveQuery;
+        }
+
+        return 1;
+
     }
 }
