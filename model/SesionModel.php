1102c337fbf1e8885a1422a44daabf605e637bda
Luis Agustin Vega Dobal <lvegadobal@alumno.unlam.edu.ar>
2023-05-28 00:51:10 -0300

    arreglo del hash

--- model/SesionModel.php
+++ model/SesionModel.php
@@ -8,8 +8,15 @@ class SesionModel{
     }
 
     public function validar($email, $clave){
-        $sql = "SELECT *  FROM usuario WHERE email='".$email."' AND contrasenia='".$clave."' AND estado='1'";
-        return $this->database->query($sql);
-    }
+        $consultaSQL = "SELECT * FROM usuario WHERE email='".$email."' AND estado='1'";
+        $saveQuery=$this->database->query($consultaSQL);
+        $contraseniaHashedaObtenida=$saveQuery["0"]["contrasenia"];
 
+        if(password_verify($clave,$contraseniaHashedaObtenida)) {
+            $sql = "SELECT * FROM usuario WHERE contrasenia='" . $contraseniaHashedaObtenida ."'";
+            return $this->database->query($sql);
+
+        }
+        return null;
+    }
 }
